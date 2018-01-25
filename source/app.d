0662890b1f512840c3f079af0ebaa9b274f6c9d1
marc fuentes <marc.fuentes@inria.fr>
2018-01-25 22:54:14 +0000

    Manage to change the color of the widget entry for now we do not now if we will keep the Entry widget or redefined our version from Label or Widget

--- source/app.d
+++ source/app.d
@@ -4,28 +4,28 @@ import gtk.Main;
 
 import gdk.Event;
 import gtk.Widget;
+import gtk.Entry;
+import gtk.Box;
+import gdk.Color;
+import gtk.Label;
+import gtk.c.types;
+
 void main(string[] args)
 {
     Main.init(args);
-    MainWindow win = new MainWindow("Egun on");
-    win.add(new QuitButton("Egun on"));
+    MainWindow win = new MainWindow("Komandoa : ");
+    win.add(new TextLine("one sample text"));
     win.showAll();
     Main.run();
 }
 
-class QuitButton : Button
+class TextLine : Entry
 {
     this(in string text)
     {
         super(text);
         modifyFont("Arial", 14);
-        addOnButtonRelease(&quit);
-    }
-
-    private bool quit(Event event, Widget widget)
-    {
-        Main.quit();
-        return true;
+        modifyFg(StateType.NORMAL, new Color(0xFF,00,0xFF));
+        modifyBg(StateType.NORMAL, new Color(0x00,00,0x00));
     }
 }
-
